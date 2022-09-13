6db593beef8c2a6db1677e835a0f6c45ce3bd26f
headbwoi <tochukwujohn24@gmail.com>
2022-09-13 03:40:26 +0100

    finished config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      dark_Mode_Background: "hsl(207, 26%, 17%)",
+      light_Mode_Background: "hsl(0, 0%, 98%)",
+      dark_Mode_Elements: "hsl(209, 23%, 22%)",
+      light_Mode_Elements: "hsl(0, 0%, 100%)",
+      dark_Mode_Text: "hsl(0, 0%, 100%)",
+      light_Mode_Text: "hsl(200, 15%, 8%)",
+      light_Mode_Input: "hsl(0, 0%, 52%)",
+    },
     extend: {},
   },
   plugins: [],

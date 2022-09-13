f65e3f8c70d969b341ebe51fcd5179a7b2d57d3b
headbwoi <tochukwujohn24@gmail.com>
2022-09-13 23:33:45 +0100

    created country card component

---
+++ components/CountryCard.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type Props = {}
+
+const CountryCard = (props: Props) => {
+  return (
+    <div className="card bg-light_Mode_Elements dark:bg-dark_Mode_Elements h-[672px] w-[535px] lg:h-[342px] lg:w-[270px] rounded-lg mx-auto lg:mx-0">
+      CountryCard
+    </div>
+  )
+}
+
+export default CountryCard

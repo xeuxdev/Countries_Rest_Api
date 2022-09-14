3563ac77d28280b86aef3d1e7e32af69709f00ba
headbwoi <tochukwujohn24@gmail.com>
2022-09-14 02:34:18 +0100

    added link to details page in country card component created dynamic rout file

---
+++ pages/[id].tsx
@@ -0,0 +1,67 @@
+import React from "react"
+
+type Props = {}
+
+const CountryDetails = (props: Props) => {
+  return <div>CountryDetailse</div>
+}
+
+export default CountryDetails
+
+/*export async function getStaticPaths() {
+  // When this is true (in preview environments) don't
+  // prerender any static pages
+  // (faster builds, but slower initial page load)
+  if (process.env.SKIP_BUILD_STATIC_GENERATION) {
+    return {
+      paths: [],
+      fallback: 'blocking',
+    }
+  }
+
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://.../posts')
+  const posts = await res.json()
+
+  // Get the paths we want to prerender based on posts
+  // In production environments, prerender all pages
+  // (slower builds, but faster initial page load)
+  const paths = posts.map((post) => ({
+    params: { id: post.id },
+  }))
+
+  // { fallback: false } means other routes should 404
+  return { paths, fallback: false }
+} */
+/*// posts will be populated at build time by getStaticProps()
+function Blog({ posts }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('https://.../posts')
+  const posts = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default Blog */
+//https://restcountries.com/v2/all?fields=alpha3Code,name,flags,population,region,capital
+//new params native name, population, region, subregion, capital, top-level-domians currencies languages border-countries

466e33f4043cce6a5bb7f470d8943efaca859b56
headbwoi <tochukwujohn24@gmail.com>
2022-09-16 13:37:36 +0100

    added search and filter functionality , removed unused codes removed env file

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,15 +11,52 @@ import { Country } from "../types/Country"
 
 const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
   const [searchQuery, setSearchQuery] = useState("")
+  const [filteredOption, setFilteredOption] = useState("")
   const [showFilterOption, setShowFilterOption] = useState(false)
 
-  const region: string[] = ["africa", "america", "asia", "europe", "oceania"]
+  const region: string[] = [
+    "all",
+    "africa",
+    "americas",
+    "asia",
+    "europe",
+    "oceania",
+  ]
 
+  const filteredCountries =
+    filteredOption === ""
+      ? countries
+      : countries.filter(
+          (country) => country.region.toLowerCase() === filteredOption
+        )
+
+  const searchedCountries =
+    searchQuery === ""
+      ? filteredCountries
+      : filteredCountries.filter((country) =>
+          country.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+
+  const cardList = searchedCountries.map((country) => {
+    return (
+      <div key={country.alpha3Code + country.name}>
+        <CountryCard data={country} />
+      </div>
+    )
+  })
+
+  const handleFilterByRegion = (region: string) => {
+    if (region === "all") {
+      setFilteredOption("")
+    } else {
+      setFilteredOption(region)
+    }
+    closeFilterMenu()
+  }
   const closeFilterMenu = () => setShowFilterOption(false)
 
   const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value)
-    console.log(searchQuery)
+    setSearchQuery(e.currentTarget.value)
   }
 
   return (
@@ -37,7 +74,7 @@ const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
       <div className="px-5 md:px-7 lg:px-6 xl:px-14 font-nunito">
         <section className="flex md:items-start flex-col space-y-10 md:space-y-0 md:flex-row md:justify-between mb-10">
           {/* input */}
-          <div className="input h-16 w-full md:w-[480px] shadow-md cursor-pointer relative">
+          <div className="input h-16 w-full md:w-96 lg:w-[480px] shadow-md cursor-pointer relative">
             <input
               type="text"
               className="w-full h-full bg-light_Mode_Elements dark:bg-dark_Mode_Elements cursor-pointer pr-10 pl-20 outline-none text-sm text-light_Mode_Text dark:text-dark_Mode_Text"
@@ -58,42 +95,36 @@ const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
                 className="bg-light_Mode_Elements dark:bg-dark_Mode_Elements w-full h-16 flex items-center justify-between px-7 shadow-md rounded-md"
                 onClick={() => setShowFilterOption(!showFilterOption)}
               >
-                <p className="text-xs font-semibold select-none">
-                  Filter by Region
+                <p className="text-base font-semibold select-none capitalize">
+                  {filteredOption === "" ? "filter by region" : filteredOption}
                 </p>
                 <ArrowDown />
               </motion.div>
 
               {showFilterOption && (
-                <AnimatePresence
-                  initial={false}
-                  mode={"wait"}
-                  onExitComplete={() => null}
+                <motion.div
+                  className="w-full absolute top-[4.5rem] px-7 py-5 bg-light_Mode_Elements dark:bg-dark_Mode_Elements flex flex-col shadow-md rounded-md"
+                  initial={{ opacity: 0, y: -50 }}
+                  animate={{ opacity: 1, y: 0, transition: { delay: 0.5 } }}
+                  exit={{ opacity: 0, transition: { delay: 0.5 } }}
                 >
-                  <motion.div
-                    className="w-full absolute top-[4.5rem] px-7 py-5 bg-light_Mode_Elements dark:bg-dark_Mode_Elements flex flex-col shadow-md rounded-md"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0, transition: { delay: 0.5 } }}
-                  >
-                    {region.map((reg) => (
-                      <li key={reg} className="list-none capitalize ">
-                        <RegionLink name={reg} />
-                      </li>
-                    ))}
-                  </motion.div>
-                </AnimatePresence>
+                  {region.map((reg) => (
+                    <li
+                      key={reg}
+                      className="list-none capitalize block text-light_Mode_Text dark:text-dark_Mode_Text text-lg font-light dark:hover:text-dark_Mode_Text/50 hover:text-light_Mode_Text/50 duration-300"
+                      onClick={() => handleFilterByRegion(reg)}
+                    >
+                      {reg}
+                    </li>
+                  ))}
+                </motion.div>
               )}
             </div>
           </ClickAwayListener>
         </section>
 
         <section className="country-cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12 md:gap-14 lg:gap-16 xl:gap-[4.6875rem] place-items-center lg:place-items-start">
-          {countries.map((country) => (
-            <div key={country.alpha3Code + country.name}>
-              <CountryCard data={country} />
-            </div>
-          ))}
+          {cardList}
         </section>
       </div>
     </>
@@ -102,19 +133,6 @@ const Home: NextPage<{ countries: Country[] }> = ({ countries }) => {
 
 export default Home
 
-type Link = {
-  name: string
-}
-
-const RegionLink = ({ name }: Link) => {
-  return (
-    <Link href={`/${name}`}>
-      <a className="block capitalise text-light_Mode_Text dark:text-dark_Mode_Text text-lg font-light dark:hover:text-dark_Mode_Text/50 hover:text-light_Mode_Text/50 duration-300">
-        {name}
-      </a>
-    </Link>
-  )
-}
 const BASE_URL = "https://restcountries.com/v2/all"
 
 export const getStaticProps = async () => {
